8dff6ef02a1bd9b034012245407055b07f212a47
Tao Xiang <46348901+yunshu67@users.noreply.github.com>
2022-03-12 23:46:21 +0100

    toolkit: create embedding layer from pretrained embeddings

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-import torch
-import torch.nn as nn
+from toolkit import *
 
-rnn = nn.RNN(input_size=10,hidden_size=20,num_layers=2,batch_first=True)
-inputs = torch.randn(3, 5, 10)  # batch_size,seq_length,embedding_dim
-h0 = torch.randn(2, 3, 20)      # D*num_layers, batch_size, hidden_size
-output, hn = rnn(inputs, h0)
+embed_layer, weights_matrix, word2idx, embed_dim = word_embedding('./glove.6B.50d.txt', True, freeze=True)
+word = 'hello'
+idx = word2idx[word]
+embedding = weights_matrix[idx]
 
-print(output)                   # batch_size,seq_length,D*hidden_size
-print(hn)                       # D*num_layers,batch_size,hidden_size
-print(output.size())
-print(hn.size())
+print(idx)
+print(embedding)
+print(type(embedding))
+
+
+data = torch.tensor([[1,2],[6,7]])
+output= embed_layer(data)
+print(output)
